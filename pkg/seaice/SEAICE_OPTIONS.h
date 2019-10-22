ec67ba3206bed4fdc152f269d3283578884d57c5
Damien Ringeisen <damien.ringeisen@awi.de>
2019-10-22 14:55:06 +0200

    add SEAICE_OPTION for teardrop

--- pkg/seaice/SEAICE_OPTIONS.h
+++ pkg/seaice/SEAICE_OPTIONS.h
@@ -115,6 +115,8 @@ C     solvers (Lemieux and Tremblay 2009, JGR). No effect on EVP
 # define SEAICE_ZETA_SMOOTHREG
 C     allow the truncated ellipse rheology (runtime flag SEAICEuseTEM)
 # undef SEAICE_ALLOW_TEM
+C     allow the teardrop and parabolic lens  rheology (runtime flag SEAICEuseTD and SEAICEusePL)
+# undef SEAICE_ALLOW_TEARDROP
 C     Use LSR vector code; not useful on non-vector machines, because it
 C     slows down convergence considerably, but the extra iterations are
 C     more than made up by the much faster code on vector machines. For
